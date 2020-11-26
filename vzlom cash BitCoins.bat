c3fff4a28206ae7166409f6c8d83e02fbc4be6f9
Windows-Src <ilyalitvinenko10@mail.ru>
2020-11-26 21:51:39 +0700

    Create vzlom cash BitCoins.bat

---
+++ vzlom cash BitCoins.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+Title Hack 1.0.miner BitCoin
+@ECHO OFF
+color 2
+@ECHO OFF
+pause
+@echo off
+Title Hacking Procesing 1.0 step 1/5
+Dir /a
+pause
+@ECHO OFF
+Title Hacking procesing 1.0 step 2/5
+Dir /s
+pause
+@ECHO OFF
+Title Hacking pocesing 1.0 step 3/5
+echo HACKIN..HACKING
+pause
+@ECHO OFF
+Title Hacking pocesing 1.0 step 4/5
+Dir /a
+@ECHO OFF
+Title Hacking procesing 1.0 step 5/5
+pause
+@ECHO OFF
+echo the hacking is finished
+echo your cache is 1.000.000 BitCoins
+echo You hacked the us President you were able to
+pause
