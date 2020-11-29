c4c24b9525a5102e53defa5b151aba554d62a642
Windows-Src <72339256+Windows-Src@users.noreply.github.com>
2020-11-29 22:22:09 +0700

    Update vzlom cash BitCoins.bat

--- vzlom cash BitCoins.bat
+++ vzlom cash BitCoins.bat
@@ -5,7 +5,7 @@ color 2
 @ECHO OFF
 pause
 @echo off
-Title Hacking Procesing 1.0 step 1/5
+Title Hacking procesing 1.0 step 1/5
 Dir /a
 pause
 @ECHO OFF
@@ -13,11 +13,11 @@ Title Hacking procesing 1.0 step 2/5
 Dir /s
 pause
 @ECHO OFF
-Title Hacking pocesing 1.0 step 3/5
+Title Hacking procesing 1.0 step 3/5
 echo HACKIN..HACKING
 pause
 @ECHO OFF
-Title Hacking pocesing 1.0 step 4/5
+Title Hacking procesing 1.0 step 4/5
 Dir /a
 @ECHO OFF
 Title Hacking procesing 1.0 step 5/5
